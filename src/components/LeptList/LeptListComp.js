a76d888f9cd66de679cc5ed1bc45be29b8c58add
James Demiraiakian <james.armen@gmail.com>
2022-02-01 12:26:54 -0800

    have a list of butter-moths rendering

--- src/components/LeptList/LeptListComp.js
+++ src/components/LeptList/LeptListComp.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-export default function LeptListComp() {
-  return <div></div>;
+export default function LeptListComp({ lept }) {
+  return (
+    <div>
+      <img src={lept.taxon.default_photo.square_url} />
+    </div>
+  );
 }
--- src/views/LeptList/LeptListView.js
+++ src/views/LeptList/LeptListView.js
@@ -1,9 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { Link, Route } from 'react-router-dom';
+import LeptListComp from '../../components/LeptList/LeptListComp';
+import { fetchLept } from '../../services/fetch';
+import LeptDetailView from '../LeptDetail/LeptDetailView';
 
 export default function LeptListView() {
-  const [lept, setLept] = useState([]);
+  const [leptList, setLeptList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {});
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchLept();
+      setLeptList(resp);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
-  return <div></div>;
+  if (loading) return <h1>LOADING...</h1>;
+
+  return (
+    <div>
+      {console.log(leptList.results)}
+      {leptList.results.map((lept) => {
+        return (
+          <Link key={lept.taxon.id} to={`/lept/${lept.taxon.id}`}>
+            <LeptListComp lept={lept} />;
+          </Link>
+        );
+      })}
+      <Route to={`/lept/:leptId`}>
+        <LeptDetailView />
+      </Route>
+    </div>
+  );
 }
